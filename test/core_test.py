5e7901798abacee5f2376b84850affeac464adec
reorx <novoreorx@gmail.com>
2016-07-31 20:42:01 +0800

    pass core_test

--- test/core_test.py
+++ test/core_test.py
@@ -2,62 +2,105 @@
 # -*- coding: utf-8 -*-
 
 from nose.tools import eq_ as equal
-from params.core import ParamSet, Field
+from params.core import ParamSet, Field, InvalidParams
+from nose.tools import assert_raises
 
 
-class FakeParams(ParamSet):
-    id = params.IntegerField('wat are you?', required=True, min=0)
-    name = params.WordField('name should be a 1~8 length string, and is required', required=True, length=(1, 8))
-    email = params.EmailField('email should be a valid email format, and is required', required=True)
-    content = params.Field('content should be a 1~20 length string', length=(1, 20))
+def test_field_null():
+    f0 = Field(null=True)
+
+    f0.validate(None)
+    f0.validate('')
+    f0.validate(0)
+
+    f1 = Field(null=False)
+    with assert_raises(ValueError):
+        f1.validate(None)
+    with assert_raises(ValueError):
+        f1.validate('')
+    # 0 is not null, only '' and None are
+    f1.validate(0)
 
 
-def test_param():
-    data_pairs = [
-        ({
-            'id': '0',
-            'name': 'asuka',
-            'email': 'asuka@nerv.com'
-        }, 0),
-        ({
-            'id': '2',
-            'name': 'lilith',
-            'email': 'l@eva.com',
-            'content': 'with adon'
-        }, 0),
-        ({
-            'id': 'a3',
-            'name': 'ayanami',
-            'email': 'rei@nerv.com'
-        }, 1),
-        ({
-            'id': 'b3',
-            'name': 'shinjigivebackmyayanami',
-            'email': 'yikali@nerv.com'
-        }, 2),
-        ({
-            'id': 'c4',
-            'name': 'E V A',
-            'email': 'eva@god',
-            'content': 'Gainax launched a project to create a movie ending for the series in 1997. The company first released Death and Rebirth on March 15'
-        }, 4),
-        ({}, 3),
-        ({
-            'id': 'd5'
-        }, 3),
-        ({
-            'id': '999'
-        }, 2),
-        ({
-            'content': 'The project to complete the final episodes (retelling episodes 25 and 26 of the series) was completed later in 1997 and released on July 19 as The End of Evangelion. '
-        }, 4)
-    ]
+def test_field_choices():
+    f0 = Field(null=False, choices=[1, '2', ''])
 
-    for data, error_num in data_pairs:
-        yield check_param, data, error_num
+    # null=False priority is higher than choices
+    with assert_raises(ValueError):
+        f0.validate('')
+
+    f0.validate(1)
+    f0.validate('2')
+
+    with assert_raises(ValueError):
+        f0.validate('1')
+    with assert_raises(ValueError):
+        f0.validate(2)
 
 
-def check_param(data, error_num):
-    params = FakeParams(data, raise_if_invalid=False)
-    print error_num, len(params.errors), params.errors
-    assert error_num == len(params.errors)
+def test_field_key():
+    class P(ParamSet):
+        f0 = Field(key='0f')
+
+    d = {'0f': 1, 'f0': 2}
+    p = P(d)
+
+    equal(p.f0, d['0f'])
+
+
+def test_field_required():
+    class P(ParamSet):
+        f0 = Field(required=True)
+        f1 = Field()
+
+    d = {'f0': 0, 'f1': 1}
+    p = P(d)
+
+    d = {'f1': 1}
+    with assert_raises(InvalidParams):
+        p = P(d)
+        print p
+
+
+def test_field_default():
+    f0_default = 1
+
+    class P(ParamSet):
+        f0 = Field(required=True, default=f0_default)
+        f1 = Field()
+
+    d = {'f0': 0}
+    p = P(d)
+    assert p.f0 != f0_default
+
+    d = {'f1': 1}
+    # even with default, required still means there should be value pass in
+    with assert_raises(InvalidParams):
+        p = P(d)
+        print p
+
+
+def test_paramset_to_dict():
+    class P(ParamSet):
+        f0 = Field()
+        f1 = Field(default=1)
+
+    d = {'f0': 0, 'f1': 1, 'f2': 2}
+    p = P(d)
+    equal(set(p.to_dict().keys()), {'f0', 'f1'})
+
+    d = {'f0': 0}
+    p = P(d)
+    # default will be involved in to_dict result
+    equal(set(p.to_dict().keys()), {'f0', 'f1'})
+
+
+def test_paramset_has():
+    class P(ParamSet):
+        f0 = Field()
+        f1 = Field(default=1)
+
+    d = {'f0': 0}
+    p = P(d)
+    assert p.has('f0')
+    assert not p.has('f1')
